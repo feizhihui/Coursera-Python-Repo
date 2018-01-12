fec41a3d0148b2f851e1e7221a6039796708087d
feizhihui <helloworld@csu.edu.cn>
2018-01-12 21:49:01 +0800

    add lecture 4 homework

---
+++ lecture_4/MovieLens_rating.py
@@ -0,0 +1,15 @@
+# encoding=utf-8
+
+import pandas as pd
+
+user = pd.read_csv('./ml-100k/ml-100k/u.user', sep='|', header=None)
+user.columns = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
+data = pd.read_csv('./ml-100k/ml-100k/u.data', sep='\t', header=None)
+data.columns = ['user_id', 'item_id', 'rating', 'timestamp']
+print(user.head())
+print(data.head())
+
+data = pd.merge(user, data, on='user_id')
+print(data.head())
+data = data.groupby(by='user_id').rating.mean()
+print(data.head())
